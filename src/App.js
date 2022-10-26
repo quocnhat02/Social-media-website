85cb7a2c0b987c9917cfe171a56531d461950428
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 17:15:27 +0700

    menu 5

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ const App = () => {
         <Navbar />
         <div style={{ display: 'flex' }}>
           <LeftBar />
-          <Outlet />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
           <RightBar />
         </div>
       </div>
