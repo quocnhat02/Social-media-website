605afccdb4a78819f8edcb10460abc997a890b66
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 16:29:01 +0700

    navbar 1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
   Outlet,
+  Navigate,
 } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import LeftBar from './components/leftBar/LeftBar';
@@ -13,6 +14,8 @@ import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 
 const App = () => {
+  const currentUser = false;
+
   const Layout = () => {
     return (
       <div>
@@ -26,12 +29,26 @@ const App = () => {
     );
   };
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
   return (
     <Router>
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<Home />} />
           <Route path="profile/:id" element={<Profile />} />
         </Route>
