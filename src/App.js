cf5ae9d953d1e5490680caccbb44169faf5a19fc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 14:53:07 +0700

    react router dom 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import LeftBar from './components/leftBar/LeftBar';
+import RightBar from './components/rightBar/RightBar';
 
 const App = () => {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: 'flex' }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       <Router>
