e2ba179c92fafd7bd561b986c0a155f05b9cfb87
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 20:04:10 +0700

    dark mode 1

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ import LeftBar from './components/leftBar/LeftBar';
 import RightBar from './components/rightBar/RightBar';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
+import './style.scss';
 
 const App = () => {
   const currentUser = true;
 
   const Layout = () => {
     return (
-      <div>
+      <div className="theme-dark">
         <Navbar />
         <div style={{ display: 'flex' }}>
           <LeftBar />
