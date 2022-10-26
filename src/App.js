0f83100efcc06a3abaad9ecebf11da8170ad7f23
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 14:23:25 +0700

    register page design 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 const App = () => {
   return (
     <>
-      <Login />
+      <Register />
     </>
   );
 };
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,35 @@ import React from 'react';
 import './register.scss';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <div className="register">
+        <div className="card">
+          <div className="left">
+            <h1>Nhat Social.</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Quisquam, architecto reiciendis, nam hic, nihil ipsum voluptatum
+              earum repellat nulla beatae eum laudantium quae harum dignissimos
+              fugiat incidunt odio quas aut.
+            </p>
+            <span>Do you have an account?</span>
+            <button>Login</button>
+          </div>
+          <div className="right">
+            <h1>Register</h1>
+            <form action="">
+              <input type="text" placeholder="Username" />
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <input type="text" placeholder="Name" />
+              <button>Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
