f55905dfc152b152384674b7726cf24b7fa6dc50
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-10-26 13:27:46 +0700

    login page design 1

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam,
+            architecto reiciendis, nam hic, nihil ipsum voluptatum earum
+            repellat nulla beatae eum laudantium quae harum dignissimos fugiat
+            incidunt odio quas aut.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
